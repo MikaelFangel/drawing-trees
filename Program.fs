e6c4abb775a2283bbda1ec8f326bfbe1b87ec7ea
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2024-06-06 12:52:47 +0200

    add merge functions
    (cherry picked from commit 10a7fdb0ea637a2014b17b97a4a30f10286a749e)

--- Program.fs
+++ Program.fs
@@ -1,2 +1,11 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿type Tree<'a> = Node of 'a * 'a Tree list
+type Extent = (float * float) list
+
+let rec merge =
+    function
+    | ([], qs) -> qs
+    | (ps, []) -> ps
+    | ((p, _) :: ps, (_, q) :: qs) -> (p, q) :: merge (ps, qs)
+
+let mergelist es =
+    List.fold (fun s e -> merge e :: s) [] es
