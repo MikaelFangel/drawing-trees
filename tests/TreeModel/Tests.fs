972b7bc2a77090e0d50dac8acb6527e4e85c6c47
Cem Akarsubaşı <cemakarsubasi@gmail.com>
2024-06-07 13:09:29 +0200

    Add FsCheck Xunit

--- tests/TreeModel/Tests.fs
+++ tests/TreeModel/Tests.fs
@@ -1,14 +1,21 @@
 module public Tests
 
-open Xunit
 open FsCheck
+open FsCheck.Xunit
 
 open TreeModel
 
 
-
-[<Fact>]
+[<Property>]
 let public ``Test`` () =
-    let tree2 = Node('a', [Node('b', []); Node('c', []); Node('d', [])])
+    let tree2 = Node('a', [ Node('b', []); Node('c', []); Node('d', []) ])
     let result = design tree2
-    Assert.True(((=) result (Node(('a', 0.0),[Node (('b', -1.0), []); Node (('c', 0.0), []); Node (('d', 1.0), [])]),[(0.0, 0.0); (-1.0, 1.0)])))
+
+    let prop =
+        fun () ->
+            ((=)
+                result
+                (Node(('a', 0.0), [ Node(('b', -1.0), []); Node(('c', 0.0), []); Node(('d', 1.0), []) ]),
+                 [ (0.0, 0.0); (-1.0, 1.0) ]))
+
+    Check.Quick prop
