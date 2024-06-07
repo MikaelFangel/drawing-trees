fc8689dba8868ef01d472104dc952fa6e6c9d801
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2024-06-07 12:54:16 +0200

    decompose the draw tree function

--- src/TreeView/View.fs
+++ src/TreeView/View.fs
@@ -3,26 +3,21 @@ module TreeView
 open TreeModel
 open Plotly.NET
 
+let createPoint a pos depth parent =
+    Chart.Point(
+        [ (pos + parent, depth) ],
+        MultiText = [ a ],
+        MultiTextPosition = [ StyleParam.TextPosition.TopCenter ],
+        ShowLegend = false
+    )
+
 let rec drawTreeH (pos_tree: Tree<'a * float>, extent) depth parent =
     match (pos_tree, depth) with
     | (Node((a, pos), subs), 0.0) ->
-        let elem =
-            Chart.Point(
-                [ (pos + parent, depth) ],
-                MultiText = [ a ],
-                MultiTextPosition = [ StyleParam.TextPosition.TopCenter ],
-                ShowLegend = false
-            )
-
+        let elem = createPoint a pos depth parent
         List.fold (fun a e -> List.append (drawTreeH (e, extent) (depth + 1.0) (pos + parent)) a) [ elem ] subs
     | (Node((a, pos), subs), depth) ->
-        let e1 =
-            Chart.Point(
-                [ (pos + parent, -depth) ],
-                MultiText = [ a ],
-                MultiTextPosition = [ StyleParam.TextPosition.TopCenter ],
-                ShowLegend = false
-            )
+        let e1 = createPoint a pos -depth parent
 
         let e2 =
             Chart.Line(
@@ -33,9 +28,6 @@ let rec drawTreeH (pos_tree: Tree<'a * float>, extent) depth parent =
             )
 
         List.fold (fun a e -> List.append (drawTreeH (e, extent) (depth + 1.0) (pos + parent)) a) [ e1; e2 ] subs
-
-
-
 
 let rec drawTree design =
     drawTreeH design 0.0 0.0 |> Chart.combine |> Chart.show
