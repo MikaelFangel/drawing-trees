4413627a351c57f455d4c99a45c13b9af72c22ce
NyMFB42 <nynne4000@hotmail.com>
2024-06-07 15:49:30 +0200

    Update View.fs

--- src/TreeView/View.fs
+++ src/TreeView/View.fs
@@ -39,33 +39,33 @@ let createVLine pos depth =
         ShowLegend = false
     )
 
-// let rec drawTreeH (pos_tree: Tree<'a * float>) depth parent =
-//     match (pos_tree, depth) with
-//     | (Node((a, pos), subs), 0.0) ->
-//         let e1 = createPoint a pos depth parent
-//         let e2 = createVLine pos depth
-//         List.fold (fun a x -> List.append (drawTreeH x (depth + 1.0) (pos + parent)) a) [ e1;e2] subs
-//     | (Node((a, pos), []), depth) ->
-//         let e1 = createPoint a pos -depth parent
-//         let e2 = createVLine (pos+parent) (-depth+0.5)
-//         let e3 = createHLine pos (-depth+0.5) parent
-//         [e1;e2;e3]
-//     | (Node((a, pos), subs), depth) ->
-//         let e1 = createPoint a pos -depth parent
-//         let e2 = createVLine (pos+parent) (-depth+0.5)
-//         let e3 = createHLine pos (-depth+0.5) parent
-//         let e4 = createVLine (pos+parent) -depth   
-//         List.fold (fun a x -> List.append (drawTreeH x (depth + 1.0) (pos + parent)) a) [e1;e2;e3;e4] subs
-
 let rec drawTreeH (pos_tree: Tree<'a * float>) depth parent =
     match (pos_tree, depth) with
     | (Node((a, pos), subs), 0.0) ->
-        let elem = createPoint a pos depth parent
-        List.fold (fun a x -> List.append (drawTreeH x (depth + 1.0) (pos + parent)) a) [ elem ] subs
+        let e1 = createPoint a pos depth parent
+        let e2 = createVLine pos depth
+        List.fold (fun a x -> List.append (drawTreeH x (depth + 1.0) (pos + parent)) a) [ e1;e2] subs
+    | (Node((a, pos), []), depth) ->
+        let e1 = createPoint a pos -depth parent
+        let e2 = createVLine (pos+parent) (-depth+0.5)
+        let e3 = createHLine pos (-depth+0.5) parent
+        [e1;e2;e3]
     | (Node((a, pos), subs), depth) ->
         let e1 = createPoint a pos -depth parent
-        let e2 = createParentLine pos -depth parent
-        List.fold (fun a x -> List.append (drawTreeH x (depth + 1.0) (pos + parent)) a) [ e1; e2 ] subs
+        let e2 = createVLine (pos+parent) (-depth+0.5)
+        let e3 = createHLine pos (-depth+0.5) parent
+        let e4 = createVLine (pos+parent) -depth   
+        List.fold (fun a x -> List.append (drawTreeH x (depth + 1.0) (pos + parent)) a) [e1;e2;e3;e4] subs
+
+// let rec drawTreeH (pos_tree: Tree<'a * float>) depth parent =
+//     match (pos_tree, depth) with
+//     | (Node((a, pos), subs), 0.0) ->
+//         let elem = createPoint a pos depth parent
+//         List.fold (fun a x -> List.append (drawTreeH x (depth + 1.0) (pos + parent)) a) [ elem ] subs
+//     | (Node((a, pos), subs), depth) ->
+//         let e1 = createPoint a pos -depth parent
+//         let e2 = createParentLine pos -depth parent
+//         List.fold (fun a x -> List.append (drawTreeH x (depth + 1.0) (pos + parent)) a) [ e1; e2 ] subs
 
 let rec drawTree design = 
     let (postree, extent) = design
