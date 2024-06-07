54a3e36108c4b7ded0655de8042660434e9226ec
Cem Akarsubaşı <cemakarsubasi@gmail.com>
2024-06-07 13:15:04 +0200

    Add a Testrunner (#9)
    * Translate code
    
    * Create module, fix merge
    
    * Delete Program.fs
    
    * Add testing, wrap functionality
    
    * Add FsCheck Xunit

--- tests/TreeModel/Tests.fs
+++ tests/TreeModel/Tests.fs
@@ -1,2 +1,21 @@
-module Tests
+module public Tests
 
+open FsCheck
+open FsCheck.Xunit
+
+open TreeModel
+
+
+[<Property>]
+let public ``Test`` () =
+    let tree2 = Node('a', [ Node('b', []); Node('c', []); Node('d', []) ])
+    let result = design tree2
+
+    let prop =
+        fun () ->
+            ((=)
+                result
+                (Node(('a', 0.0), [ Node(('b', -1.0), []); Node(('c', 0.0), []); Node(('d', 1.0), []) ]),
+                 [ (0.0, 0.0); (-1.0, 1.0) ]))
+
+    Check.Quick prop
