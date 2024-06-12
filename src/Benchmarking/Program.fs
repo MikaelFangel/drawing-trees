467e554fc97d8e7197d910ce6ef963af2a1ca3c9
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2024-06-12 23:23:21 +0200

    add benchmark to tikz rendering

--- src/Benchmarking/Program.fs
+++ src/Benchmarking/Program.fs
@@ -8,22 +8,33 @@ let rec buildTree =
     | 0 -> Node(1, [])
     | n -> Node(1, [ buildTree (n - 1); buildTree (n - 1); buildTree (n - 1) ])
 
-type TreeModelBenchmarking() =
+type TreeBenchmarking() =
     let mutable tree = None
+    let mutable dtree = None
 
     [<Params(1, 2, 5, 10)>]
     member val TreeSize = 0 with get, set
 
-    [<GlobalSetup>]
-    member this.Setup() = tree <- Some(buildTree this.TreeSize)
+    [<GlobalSetup(Target = "DesignTree")>]
+    member this.SetupDesignTree() = tree <- Some(buildTree this.TreeSize)
+
+    [<GlobalSetup(Target = "RenderTikz")>]
+    member this.SetupRenderTikz() =
+        dtree <- Some(buildTree 5 |> TreeModel.design)
 
     [<Benchmark>]
     member _.DesignTree() =
         match tree with
-        | Some tree -> TreeModel.design tree
+        | Some tree -> TreeModel.design tree |> ignore
+        | None -> failwith "No tree to benchmark."
+
+    [<Benchmark>]
+    member _.RenderTikz() =
+        match dtree with
+        | Some dtree -> TreeView.Tikz.drawTree dtree |> ignore
         | None -> failwith "No tree to benchmark."
 
 [<EntryPoint>]
 let main argv =
-    BenchmarkRunner.Run<TreeModelBenchmarking>() |> ignore
+    BenchmarkRunner.Run<TreeBenchmarking>() |> ignore
     0
