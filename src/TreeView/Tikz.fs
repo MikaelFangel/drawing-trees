a90d9c43e36fac033bf5cb4b5255962e269c35b3
Cem Akarsubaşı <cemakarsubasi@gmail.com>
2024-06-12 11:44:02 +0200

    Make Tikz string concatenation lazy (#30)
    * Lazily construct sequence of strings for performance
    
    * Factor out a function

--- src/TreeView/Tikz.fs
+++ src/TreeView/Tikz.fs
@@ -5,42 +5,46 @@ module TreeView.Tikz
 open TreeModel
 
 let private createPoint name label pos depth parent =
-    $"\\node ({name}) at ({pos+parent}, {depth}) {{{label}}};\n"
+    $"\\node ({name}) at ({pos + parent}, {depth}) {{{label}}};\n"
 
 let private createLine start finish =
     $"\\draw [thick] ({start}) -- ({finish});\n"
 
 let rec private drawTreeH (pos_tree: Tree<'a * float>) depth parent =
+    let drawTreeH' subs toappend pos =
+        subs
+        |> Seq.map (fun tree -> drawTreeH tree (depth + 1.0) (pos + parent))
+        |> Seq.concat
+        |> Seq.append toappend
+
     match (pos_tree, depth) with
     // top node
     | (Node((a, pos), subs), 0.0) ->
-        let e1 = createPoint a a pos (-depth) parent
-        let vline = createLine a $"{parent+pos}, {-depth-0.5}"
-        let folder acc subtree = acc + (drawTreeH subtree (depth + 1.0) (pos + parent))
-        List.fold folder (e1 + vline + "\n") subs
+        let point = createPoint a a pos (-depth) parent
+        let vline = createLine a $"{parent + pos}, {-depth - 0.5}"
+        drawTreeH' subs [ point; vline; "\n" ] pos
     // bottom node
     | (Node((a, pos), []), depth) ->
-        let e1 = createPoint a a pos (-depth) parent
-        let vline = createLine a $"{parent+pos}, {-depth+0.5}"
-        let hline = createLine $"{parent}, {0.5-depth}" $"{parent+pos}, {0.5-depth}"
-        e1  + vline + hline + "\n"
+        let point = createPoint a a pos (-depth) parent
+        let vline = createLine a $"{parent + pos}, {-depth + 0.5}"
+        let hline = createLine $"{parent}, {0.5 - depth}" $"{parent + pos}, {0.5 - depth}"
+        [ point; vline; hline; "\n" ]
     // vers node
     | (Node((a, pos), subs), depth) ->
-        let e1 = createPoint a a pos (-depth) parent
-        let vline_top = createLine a $"{parent+pos}, {-depth+0.5}"
-        let vline_bot = createLine a $"{parent+pos}, {-depth-0.5}"
-        let hline = createLine $"{parent}, {0.5-depth}" $"{parent+pos}, {0.5-depth}"
-        let folder acc subtree = acc + (drawTreeH subtree (depth + 1.0) (pos + parent))
-        List.fold folder (e1 + vline_top + vline_bot + hline + "\n") subs
-    
-let private preamble =
-    "\\begin{tikzpicture}\n"
+        let point = createPoint a a pos (-depth) parent
+        let vline_top = createLine a $"{parent + pos}, {-depth + 0.5}"
+        let vline_bot = createLine a $"{parent + pos}, {-depth - 0.5}"
+        let hline = createLine $"{parent}, {0.5 - depth}" $"{parent + pos}, {0.5 - depth}"
+        drawTreeH' subs [ point; vline_top; vline_bot; hline; "\n" ] pos
 
-let private postamble = 
-    "\\end{tikzpicture}\n"
+let private preamble = "\\begin{tikzpicture}\n"
+
+let private postamble = "\\end{tikzpicture}\n"
 
 /// Returns a latex string that draws the graph using basic Tikz commands
-let rec drawTree design = 
-    let (postree,extent) = design
-    let center = drawTreeH postree 0.0 0.0
-    preamble + center + postamble
+let rec drawTree design =
+    let (postree, _) = design
+
+    Seq.append (drawTreeH postree 0.0 0.0) [ postamble ]
+    |> Seq.append [ preamble ]
+    |> String.concat ""
