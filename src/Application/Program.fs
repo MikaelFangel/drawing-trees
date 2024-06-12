03a2199c6ee6ea3f88a70183474dd12bc4f92fbe
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2024-06-12 11:32:49 +0200

    added example mode

--- src/Application/Program.fs
+++ src/Application/Program.fs
@@ -25,39 +25,28 @@ let rec addchildren (Node(s, sub)) =
 
 [<EntryPoint>]
 let main args =
-    let outputType = chooseOutputType ()
+    match args with
+    | [| "example"; n |] ->
+        match n with
+        | "1" -> Plotly.drawTree (design Examples.tree1)
+        | "2" -> Plotly.drawTree (design Examples.tree2)
+        | "3" -> Plotly.drawTree (design Examples.tree3)
+        | "4" -> Plotly.drawTree (design Examples.tree4)
+        | "5" -> Plotly.drawTree (design Examples.tree5)
+        | _ -> failwith "Invalid example number"
 
-    System.Console.WriteLine("Enter the label for the root of the tree:")
-    let root = System.Console.ReadLine() |> fun s -> Node(s, [])
+        0
+    | [||] ->
+        let outputType = chooseOutputType ()
 
-    let tree = addchildren root
+        System.Console.WriteLine("Enter the label for the root of the tree:")
+        let root = System.Console.ReadLine() |> fun s -> Node(s, [])
 
-    match outputType with
-    | Tikz -> System.Console.WriteLine(Tikz.drawTree (design tree))
-    | Plotly -> Plotly.drawTree (design tree)
+        let tree = addchildren root
 
-    0
+        match outputType with
+        | Tikz -> System.Console.WriteLine(Tikz.drawTree (design tree))
+        | Plotly -> Plotly.drawTree (design tree)
 
-//let tree4 = Node (1,[Node(2,[Node(2,[]);Node(3,[])]);Node(3,[Node (1,[Node(2,[]);Node(3,[])])])])
-//let tree5 = Node (1,[Node(2,[Node(2,[]);Node(3,[])]);Node(3,[Node (1,[Node(2,[]);Node(3,[])])]);Node(4,[])])
-//let (pos4,e1) = design tree4
-//let (pos5,e2) = design tree5
-//printfn "%A" pos4
-//printfn "%A" pos5
-//
-//equalTree pos4 pos5 |> printfn "%A"
-//posEqual pos4 pos5 true |> printfn "%A"
-//
-//let tree1 = Node (1,[Node(2,[]);Node(3,[]);Node(4,[])])
-//let design1 = design tree1
-//Plotly.drawTree (design1)
-//
-//let tree2 = Node ("A",[Node("B",[Node ("C",[]);Node("P",[])]);Node("S",[Node("T",[]);Node("e",[])]);Node("o",[Node("p",[])])])
-//let design2 = design tree2
-//Plotly.drawTree (design2)
-//
-//let tree3 = Node ("A",[Node("B",[Node ("C",[]);Node("P",[])]);Node("S",[Node("T",[]);Node("e",[])]);Node("o",[Node("p",[Node("q",[Node("r",[]);Node("s",[]);Node("t",[]);Node("u",[])]);Node("v",[Node("w",[]);Node("x",[Node("y",[]);Node("z",[])]);Node("0",[]);Node("1",[])]);Node("2",[])])])])
-//let design3 = design tree3
-//Plotly.drawTree (design3)
-//
-//printf "%s" (Tikz.drawTree (design3))
+        0
+    | _ -> 1
