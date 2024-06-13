4db13cd370f63a50ab256f146dd6eb8b9579c022
Cem Akarsubaşı <cemakarsubasi@gmail.com>
2024-06-13 10:55:50 +0200

    Eliminate unnecessary string interpolation, inline functions

--- src/TreeView/Tikz.fs
+++ src/TreeView/Tikz.fs
@@ -4,11 +4,14 @@ module TreeView.Tikz
 
 open TreeModel
 
-let private createPoint name label pos depth parent =
-    $"\\node (%O{name}) at (%f{pos + parent}, %f{depth}) {{%O{label}}};\n"
+let inline private createPoint name label pos depth parent =
+    $"\\node (%O{name}) at (%.2f{pos + parent}, %.2f{depth}) {{%O{label}}};\n"
 
-let private createLine start finish =
-    $"\\draw [thick] (%O{start}) -- (%O{finish});\n"
+let inline private createLinePXY p x2 y2 = 
+    $"\\draw [thick] (%O{p}) -- (%.2f{x2}, %.2f{y2});\n"
+
+let inline private createLineXY x1 y1 x2 y2 = 
+    $"\\draw [thick] (%.2f{x1}, %.2f{y1}) -- (%.2f{x2}, %.2f{y2});\n"
 
 // Gets the left most position in a list of trees.
 let private findLeftMost =
@@ -17,53 +20,51 @@ let private findLeftMost =
     | Node((_, f), _) :: _ -> f
 
 // Gets the right most position in a list of trees.
+[<TailCall>]
 let rec private findRightMost =
     function
     | [] -> 0.0
     | [ Node((_, f), _) ] -> f
     | _ :: xs -> findRightMost xs
 
-let private createHLine subs parent pos depth =
+let inline private createHLine subs parent pos depth =
         let (left, right) = (findLeftMost subs, findRightMost subs)
         match (left, right) with
         | (0.0, 0.0) -> ""
-        | (l, r) -> createLine $"%f{l+parent+pos}, %f{-depth - 0.5}" $"%f{r+parent+pos}, %f{-depth - 0.5}" 
+        | (l, r) -> createLineXY (l+parent+pos) (-depth - 0.5) (r+parent+pos) (-depth - 0.5)
 
-let rec private drawTreeH (pos_tree: Tree<'a * float>) depth parent =
-    let drawTreeH' subs toappend pos =
-        subs
-        |> List.map (fun tree -> drawTreeH tree (depth + 1.0) (pos + parent))
-        |> List.concat
-        |> List.append toappend
+[<TailCall>]
+let rec private drawTree' (pos_tree) depth parent: string list =
+    let inline drawTreeH' subs pos =
+        subs |> List.collect (fun tree -> drawTree' tree (depth + 1.0) (pos + parent))
 
     match (pos_tree, depth) with
     // top node
     | (Node((a, pos), subs), 0.0) ->
         let point = createPoint a a pos (-depth) parent
-        let vline = createLine a $"%f{parent + pos}, %f{-depth - 0.5}"
+        let vline = createLinePXY a (parent + pos) (-depth - 0.5)
         let hline = createHLine subs parent pos depth
-        drawTreeH' subs [ point; vline; hline; "\n" ] pos
+        [ point; vline; hline ] @ drawTreeH' subs pos
     // bottom node
     | (Node((a, pos), []), depth) ->
         let point = createPoint a a pos (-depth) parent
-        let vline = createLine a $"%f{parent + pos}, %f{-depth + 0.5}"
-        [ point; vline; "\n" ]
+        let vline = createLinePXY a (parent + pos) (-depth + 0.5)
+        [ point; vline ]
     // vers node
     | (Node((a, pos), subs), depth) ->
         let point = createPoint a a pos (-depth) parent
-        let vline_top = createLine a $"%f{parent + pos}, %f{-depth + 0.5}"
-        let vline_bot = createLine a $"%f{parent + pos}, %f{-depth - 0.5}"
+        let vline_top = createLinePXY a (parent + pos) (-depth + 0.5)
+        let vline_bot = createLinePXY a (parent + pos) (-depth - 0.5)
         let hline = createHLine subs parent pos depth
-        drawTreeH' subs [ point; vline_top; vline_bot; hline; "\n" ] pos
+        [ point; vline_top; vline_bot; hline; ] @ drawTreeH' subs pos
 
 let private preamble = "\\begin{tikzpicture}\n"
 
 let private postamble = "\\end{tikzpicture}\n"
 
 /// Returns a latex string that draws the graph using basic Tikz commands
-let rec drawTree design =
+let drawTree design =
     let (postree, _) = design
-
-    List.append (drawTreeH postree 0.0 0.0) [ postamble ]
-    |> List.append [ preamble ]
+    let all = [preamble] @ (drawTree' postree 0.0 0.0) @ [postamble] 
+    all
     |> String.concat ""
